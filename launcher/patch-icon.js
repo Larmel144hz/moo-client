e03f1290e7c71299ac7846df40d806a0f1beb878
Larmel144hz <larmel144hz@users.noreply.github.com>
2026-08-15 13:41:37 +0200

    Update launcher version to 1.0.1 and attach installer exe to release

--- launcher/patch-icon.js
+++ launcher/patch-icon.js
@@ -4,10 +4,13 @@ const path = require('path');
 const exePath = path.join(__dirname, 'dist', 'win-unpacked', 'Moo Client.exe');
 const icoPath = path.join(__dirname, 'src', 'renderer', 'icon.ico');
 
+const pkg = require('./package.json');
+const ver = pkg.version || '1.0.1';
+
 rcedit(exePath, {
     icon: icoPath,
-    'product-version': '1.0.0',
-    'file-version': '1.0.0',
+    'product-version': ver,
+    'file-version': ver,
     'version-string': {
         ProductName: 'Moo Client',
         FileDescription: 'Moo Client Launcher',
