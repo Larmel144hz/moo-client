4617a8d40d6df6b6f7794e2eced0ed81f585f095
Larmel144hz <larmel144hz@users.noreply.github.com>
2026-08-15 13:46:52 +0200

    Configure full NSIS installer and uninstaller icons for Windows

--- launcher/patch-icon.js
+++ launcher/patch-icon.js
@@ -2,7 +2,7 @@ const { rcedit } = require('rcedit');
 const path = require('path');
 
 const exePath = path.join(__dirname, 'dist', 'win-unpacked', 'Moo Client.exe');
-const icoPath = path.join(__dirname, 'src', 'renderer', 'icon.ico');
+const icoPath = path.join(__dirname, 'build', 'icon.ico');
 
 const pkg = require('./package.json');
 const ver = pkg.version || '1.0.1';
